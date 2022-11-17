0450cd744d74d8df0bad18ee268902fd429c248e
João <joaohnsousa@gmail.com>
2022-11-17 13:39:53 -0300

    SnakeGame

---
+++ src/snakegame/SnakeGame.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snakegame;
+
+import com.sun.glass.ui.Cursor;
+
+/**
+ *
+ * @author User
+ */
+public class SnakeGame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       GameFrame frame = new GameFrame(); frame.setVisible(true);
+        
+    }
+    
+    
+}
